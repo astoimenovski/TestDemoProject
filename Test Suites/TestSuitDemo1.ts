88d21a5f603f8839281749ea18d56891f512fba7
astoimenovski <astoimenovski@PF36Z5LX.dss.local>
2022-01-07 10:04:25 -0500

    First Commit

---
+++ Test Suites/TestSuitDemo1.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuitDemo1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>14a44658-f421-4c72-bee8-e1491c006862</testSuiteGuid>
+   <testCaseLink>
+      <guid>f030437b-7d48-4185-808f-ee20aaf976f7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/ManualModeDemo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c9565b75-2496-415f-8ce9-01d6f1f96be2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Demo/RecordModeDemo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bf713640-c7f9-4c81-b4fe-41b249335cc8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/ScriptModeDemo</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
